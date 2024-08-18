a94e08a691732110b61dc332bcd4e57206f34b0f
geonwoo92 <cnu201102054@gmail.com>
2024-08-18 23:28:09 +0900

    0818 last

--- src/main/resources/dddd.sql
+++ src/main/resources/dddd.sql
@@ -1,1 +1,20 @@
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null , '2024-07-20 13:39:50.445810', 101, '150', '150', '300', '20', '30', '45', '55', '50', '40', '60', '94000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-07-25 13:39:50.445810', 101, '200', '200', '400', '25', '50', '60', '65', '60', '45', '95', '95000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-07-30 13:39:50.445810', 101, '240', '310', '550', '25', '60', '70', '85', '75', '50', '185', '94000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-08-01 13:39:50.445810', 101, '300', '350', '650', '30', '70', '80', '120', '90', '55', '205', '94000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-08-06 13:39:50.445810', 101, '280', '320', '600', '30', '75', '90', '85', '80', '50', '190', '94000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-08-11 13:39:50.445810', 101, '320', '370', '690', '30', '90', '100', '100', '100', '60', '170', '94000', null);
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 1, null, null, '2024-08-16 13:39:50.445810', 101, '380', '420', '800', '30', '100', '120', '130', '120', '70', '230', '94000', null);
 
+
+
+
+INSERT INTO toeicdoit_db.result_model (is_correct, category_id, created_at, id, updated_at, user_id, lc_score, rc_score, score, score_part1, score_part2, score_part3, score_part4, score_part5, score_part6, score_part7, time_elapsed, user_answer)
+VALUES (false, 2, null, null, '2024-08-16 13:39:50.445810', 101, '50', '50', '0', '0', '0', '0', '0', '0', '0', '0', '0', null);
